30ba2c9b2fe7491c39c7e3b6d2a4e9d7a98a154f
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-06 22:13:42 +0300

    ders sonu 13: Deneyimleri Listeleme

---
+++ HasanBozkusCv/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HasanBozkusCv.Models.Entity;
+
+namespace HasanBozkusCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DBCvEntities db = new DBCvEntities();
+
+        // GET: Default
+        public ActionResult Index()
+        {
+            var degerler = db.Abouts.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.Experience.ToList();
+            return PartialView(deneyimler);
+        }
+    }
+}
