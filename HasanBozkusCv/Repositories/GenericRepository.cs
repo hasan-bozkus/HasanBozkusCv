0b69933cd10ab821b1d4f2eef8aff4acf97fc3d3
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-02-12 15:46:59 +0300

    Repository Design Pattern

---
+++ HasanBozkusCv/Repositories/GenericRepository.cs
@@ -0,0 +1,40 @@
+﻿using HasanBozkusCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HasanBozkusCv.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        DBCvEntities db = new DBCvEntities();
+
+        public List<T> List()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public void TAdd(T t)
+        {
+            db.Set<T>().Add(t);
+            db.SaveChanges();
+        }
+
+        public void TDelete(T t)
+        {
+            db.Set<T>().Remove(t);
+            db.SaveChanges();
+        }
+
+        public T TGet(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public void TUpdate(T t)
+        {
+            db.SaveChanges();
+        }
+    }
+}
